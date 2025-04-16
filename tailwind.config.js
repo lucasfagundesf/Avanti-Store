4611251eac83830bb9015a0eff4b858cd6261ff5
Lucas Fagundes <lucas_fagundesfranco@hotmail.com>
2025-04-16 14:28:29 -0300

    :tada: Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/index.html', // Inclua todos os arquivos HTML na pasta src
+      './src/script.js',   // Inclua arquivos JS, se necessário
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
